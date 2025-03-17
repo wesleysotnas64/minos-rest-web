5005a9f6c2d46ec5b612ae330f6edbee82a2015c
wesleysotnas64 <wesleysotnas64@gmail.com>
2025-03-17 13:06:24 -0300

    feat: Login page created;

---
+++ src/pages/login/Login.jsx
@@ -0,0 +1,43 @@
+import {LoginArea, SplashArtArea, PageContainer, LogoArea, LoginInputArea, LoginInput} from "./Login.style";
+import bgImage from "../../assets/img/restaurant01.jpg";
+import iconLogin from "../../assets/icon/icon-login.svg"
+
+function Login() {
+
+    return (
+      <>
+        <PageContainer>
+            <SplashArtArea>
+                <img src={bgImage}></img>
+            </SplashArtArea>
+
+            <LoginArea>
+                <LogoArea>
+
+                </LogoArea>
+
+                <h1>Login</h1>
+
+                <LoginInputArea>
+                    <label>Usuário</label>
+                    <LoginInput />
+
+                    <label>Senha</label>
+                    <LoginInput />
+                </LoginInputArea>
+
+                <button>
+                    <img src={iconLogin} />
+                </button>
+
+                <footer>
+                    <h2>MinOS-Rest</h2>
+                    <p>Minimal Operational System Restaurant</p>
+                </footer>
+            </LoginArea>
+        </PageContainer>
+      </>
+    )
+  }
+  
+  export default Login
---
+++ src/themes.js
@@ -0,0 +1,10 @@
+export const theme = {
+    colors: {
+        white1: "#ffffff",
+        balck1: "#333333",
+        red1: "#ff5757",
+        red2: "#ee4646",
+        gray1: "#dedede",
+        gray2: "#766d66",
+    }
+}
